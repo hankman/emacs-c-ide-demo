072ecdead8144db61240998d787c3a5d669ff14a
Chen Fan <c-fan@outlook.com>
2022-01-26 18:19:28 +0800

    add short cut and change color

--- init.el
+++ init.el
@@ -24,24 +24,102 @@
 (require 'setup-cedet)
 (require 'setup-editing)
 
+(wn-mode)
+(load-theme 'zenburn t)
+
+(require 'powerline)
+(powerline-default-theme)
+
+(require 'nyan-mode)
+(nyan-mode)
+
+(global-set-key [(f4)] 'eshell)
+(global-set-key [(f5)] 'goto-line)
+(global-set-key [(f7)] 'compile)
+(global-set-key [(f6)] 'speedbar-get-focus)
+(global-set-key [(f9)] 'list-bookmarks)
+(global-set-key [(f10)] 'bookmark-set)
+(global-set-key [(f3)] 'dired)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; custom c++
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(c-add-style "microsoft"
+             '("stroustrup"
+               (c-offsets-alist
+                (innamespace . -)
+                (inline-open . 0)
+                (inher-cont . c-lineup-multi-inher)
+                (arglist-cont-nonempty . +)
+                (template-args-cont . +))))
+(setq c-default-style "microsoft")
 
 
-;; function-args
-;; (require 'function-args)
-;; (fa-config-default)
-;; (define-key c-mode-map  [(tab)] 'company-complete)
-;; (define-key c++-mode-map  [(tab)] 'company-complete)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#93E0E3")
+ '(cua-normal-cursor-color "#DCDCCC")
+ '(cua-overwrite-cursor-color "#F0DFAF")
+ '(cua-read-only-cursor-color "#7F9F7F")
+ '(exwm-floating-border-color "#262626")
+ '(highlight-changes-colors (quote ("#DC8CC3" "#bbb0cb")))
+ '(highlight-symbol-colors
+   (quote
+    ("#67605e255590" "#4e615e515e24" "#5f8b4d004c7a" "#55b0556a5e39" "#4ce7558e4c96" "#5f01556d4cad" "#4e615e515e24")))
+ '(highlight-symbol-foreground-color "#FFFFEF")
+ '(highlight-tail-colors
+   (quote
+    (("#4F4F4F" . 0)
+     ("#488249" . 20)
+     ("#5dacaf" . 30)
+     ("#57a2a4" . 50)
+     ("#b6a576" . 60)
+     ("#ac7b5a" . 70)
+     ("#aa5790" . 85)
+     ("#4F4F4F" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#b6a576" "#ac7b5a" "#9f5c5c" "#aa5790" "#85749c" "#57a2a4" "#5dacaf" "#488249")))
+ '(hl-fg-colors
+   (quote
+    ("#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F")))
+ '(hl-paren-colors (quote ("#93E0E3" "#F0DFAF" "#8CD0D3" "#bbb0cb" "#7F9F7F")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#8CD0D3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
+ '(lsp-ui-doc-border "#FFFFEF")
+ '(objed-cursor-color "#CC9393")
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (magit yasnippet-snippets nyan-mode zygospore zenburn-theme yasnippet ws-butler wn-mode volatile-highlights use-package undo-tree spaceline solarized-theme iedit helm-themes helm-swoop helm-projectile helm-gtags dtrt-indent dream-theme doom-themes doom-modeline company comment-dwim-2 clean-aindent-mode atom-dark-theme arc-dark-theme anzu)))
+ '(pos-tip-background-color "#4F4F4F")
+ '(pos-tip-foreground-color "#FFFFEF")
+ '(rustic-ansi-faces
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#7F9F7F" "#4F4F4F" 0.2))
+ '(term-default-bg-color "#3F3F3F")
+ '(term-default-fg-color "#DCDCCC")
+ '(vc-annotate-background-mode nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#3F3F3F" "#4F4F4F" "#9f5c5c" "#CC9393" "#488249" "#7F9F7F" "#b6a576" "#F0DFAF" "#57a2a4" "#8CD0D3" "#aa5790" "#DC8CC3" "#5dacaf" "#93E0E3" "#DCDCCC" "#6F6F6F")))
+ '(xterm-color-names
+   ["#4F4F4F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#fffff6"])
+ '(xterm-color-names-bright
+   ["#3F3F3F" "#DFAF8F" "#878777" "#6F6F6F" "#DCDCCC" "#bbb0cb" "#FFFFEF" "#FFFFFD"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(powerline-active0 ((t (:inherit mode-line :background "#010000"))))
+ '(powerline-active1 ((t (:inherit mode-line :background "color-18"))))
+ '(vertical-border ((t (:background "color-247" :foreground "color-22")))))
