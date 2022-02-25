c74e3665978620cac750f084c8efb88c66e6bed8
Chen Fan <c-fan@outlook.com>
2022-02-25 19:12:46 +0800

    add window number to mode line

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 (require 'setup-editing)
 
 (wn-mode)
+
 (load-theme 'zenburn t)
 
 (require 'powerline)
@@ -96,6 +97,126 @@
  '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
  '(lsp-ui-doc-border "#FFFFEF")
+ '(mode-line-format
+   (quote
+    ("%e"
+     (:eval
+      (let*
+          ((active
+            (powerline-selected-window-active))
+           (mode-line-buffer-id
+            (if active
+                (quote mode-line-buffer-id)
+              (quote mode-line-buffer-id-inactive)))
+           (mode-line
+            (if active
+                (quote mode-line)
+              (quote mode-line-inactive)))
+           (face0
+            (if active
+                (quote powerline-active0)
+              (quote powerline-inactive0)))
+           (face1
+            (if active
+                (quote powerline-active1)
+              (quote powerline-inactive1)))
+           (face2
+            (if active
+                (quote powerline-active2)
+              (quote powerline-inactive2)))
+           (face-hl
+            (if active
+                (quote wn-modeline-face)
+              face1))
+           (separator-left
+            (intern
+             (format "powerline-%s-%s"
+                     (powerline-current-separator)
+                     (car powerline-default-separator-dir))))
+           (separator-right
+            (intern
+             (format "powerline-%s-%s"
+                     (powerline-current-separator)
+                     (cdr powerline-default-separator-dir))))
+           (lhs
+            (list
+             (powerline-raw
+              (format "%s" (wn--selected-window-number)) face-hl (quote l))
+             (funcall separator-left face-hl face0)
+             (powerline-raw "%*" face0
+                            (quote l))
+             (when powerline-display-buffer-size
+               (powerline-buffer-size face0
+                                      (quote l)))
+             (when powerline-display-mule-info
+               (powerline-raw mode-line-mule-info face0
+                              (quote l)))
+             (powerline-buffer-id
+              (\`
+               (mode-line-buffer-id
+                (\, face0)))
+              (quote l))
+             (when
+                 (and
+                  (boundp
+                   (quote which-func-mode))
+                  which-func-mode)
+               (powerline-raw which-func-format face0
+                              (quote l)))
+             (powerline-raw " " face0)
+             (funcall separator-left face0 face1)
+             (when
+                 (and
+                  (boundp
+                   (quote erc-track-minor-mode))
+                  erc-track-minor-mode)
+               (powerline-raw erc-modified-channels-object face1
+                              (quote l)))
+             (powerline-major-mode face1
+                                   (quote l))
+             (powerline-process face1)
+             ;(powerline-minor-modes face1
+             ;                       (quote l))
+             (powerline-narrow face1
+                               (quote l))
+             (powerline-raw " " face1)
+             (funcall separator-left face1 face2)
+             (powerline-vc face2
+                           (quote r))
+             (when
+                 (bound-and-true-p nyan-mode)
+               (powerline-raw (list
+                 (nyan-create))
+                face2
+                (quote l)))))
+           (rhs
+            (list
+             (powerline-raw global-mode-string face2
+                            (quote r))
+             (funcall separator-right face2 face1)
+             (unless window-system
+               (powerline-raw
+                (char-to-string 57505)
+                face1
+                (quote l)))
+             (powerline-raw "%4l" face1
+                            (quote l))
+             (powerline-raw ":" face1
+                            (quote l))
+             (powerline-raw "%3c" face1
+                            (quote r))
+             (funcall separator-right face1 face0)
+             (powerline-raw " " face0)
+             (powerline-raw "%6p" face0
+                            (quote r))
+             (when powerline-display-hud
+               (powerline-hud face0 face2))
+             (powerline-fill face0 0))))
+        (concat
+         (powerline-render lhs)
+         (powerline-fill face2
+                         (powerline-width rhs))
+         (powerline-render rhs)))))))
  '(objed-cursor-color "#CC9393")
  '(package-selected-packages
    (quote
@@ -122,7 +243,8 @@
  ;; If there is more than one, they won't work right.
  '(powerline-active0 ((t (:inherit mode-line :background "#010000"))))
  '(powerline-active1 ((t (:inherit mode-line :background "color-18"))))
- '(vertical-border ((t (:background "color-247" :foreground "color-22")))))
+ '(vertical-border ((t (:background "color-247" :foreground "color-22"))))
+ '(wn-modeline-face ((t (:background "yellow" :foreground "color-52")))))
 
 
 ;; Setup lsp
